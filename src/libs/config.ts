7aacff29ca0a5395060e1fbad367ce6775e51ea1
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-10-01 22:13:10 -0500

    Connecting to the database

---
+++ src/libs/config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  database: {
+    type: process.env.DATABASE_TYPE || 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    db: process.env.DATABASE_DB,
+  },
+});
